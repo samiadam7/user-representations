6e2a59bcfca9741e444d6a370e97439cbad5c92b
Sami Adam <sami.adam.usa@gmail.com>
2026-01-01 23:12:11 +0300

    fixed spacing consistency issues

--- src/preprocessor/transformations.py
+++ src/preprocessor/transformations.py
@@ -10,15 +10,17 @@ from sklearn.decomposition import PCA
 def drop_na_and_duplicates(df:pd.DataFrame, subset:List[str]) -> pd.DataFrame:
     return df.dropna().drop_duplicates(subset=subset).copy()
     
-
+    
 def column_list_transform(df:pd.DataFrame, columns:List[str]) -> pd.DataFrame:
     return df[columns].apply(lambda x: ast.literal_eval(x) if isinstance(x, str) else x)
+
 
 def shuffle_data(df:pd.DataFrame, random_state:int | None = 42) -> pd.DataFrame:
     try:
         return df.sample(frac=1, random_state=random_state)
     except:
         return df.sample(frac=1)
+
 
 def select_first_item_from_list_column(df:pd.DataFrame) -> pd.DataFrame:
     return df.apply(lambda x: x[0] if isinstance(x, list) else x)
